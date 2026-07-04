c705040fd010e87d460023666c27b7f424441f98
Kazi Omar Faruk <kaziomar144@gmail.com>
2026-07-04 17:02:47 +0600

    fix: broadcast auth url

--- src/lib/echo/echo.ts
+++ src/lib/echo/echo.ts
@@ -10,36 +10,36 @@ console.log("=== DEBUG 1: echo.ts file is loaded successfully! ===");
 Pusher.logToConsole = true;
 
 // Check if ENV variables are actually loading
-console.log("=== DEBUG 2: Checking ENV Variables ===");
-console.log("HOST:", import.meta.env.VITE_REVERB_HOST);
-console.log("PORT:", import.meta.env.VITE_REVERB_PORT);
-console.log("SCHEME:", import.meta.env.VITE_REVERB_SCHEME);
-console.log("AUTH ENDPOINT:", import.meta.env.VITE_BROADCAST_AUTH_ENDPOINT);
+// console.log("=== DEBUG 2: Checking ENV Variables ===");
+// console.log("HOST:", import.meta.env.VITE_REVERB_HOST);
+// console.log("PORT:", import.meta.env.VITE_REVERB_PORT);
+// console.log("SCHEME:", import.meta.env.VITE_REVERB_SCHEME);
+// console.log("AUTH ENDPOINT:", import.meta.env.VITE_BROADCAST_AUTH_ENDPOINT);
 
 const reverbHost = import.meta.env.VITE_REVERB_HOST || "10.10.28.53";
 const reverbPort = Number(import.meta.env.VITE_REVERB_PORT || 8020);
 const authEndpoint =
   import.meta.env.VITE_BROADCAST_AUTH_ENDPOINT ||
-  "http://10.10.28.53:60/api/broadcasting/auth";
+  "https://api.lovelysmealplans.com/api/broadcasting/auth";
 const reverbAppKey =
   import.meta.env.VITE_REVERB_APP_KEY || "oskghldeokkdfjdslhnfd";
 const isTls = import.meta.env.VITE_REVERB_SCHEME === "https";
 
-console.log("=== DEBUG 3: Final Echo Config ===", {
-  reverbHost,
-  reverbPort,
-  isTls,
-  authEndpoint,
-});
+// console.log("=== DEBUG 3: Final Echo Config ===", {
+//   reverbHost,
+//   reverbPort,
+//   isTls,
+//   authEndpoint,
+// });
 
 const echo = new Echo({
   broadcaster: "reverb",
   key: reverbAppKey,
   wsHost: reverbHost,
-  wsPort: reverbPort,
-  wssPort: reverbPort,
+  wsPort: 80,
+  wssPort: 443,
   forceTLS: isTls,
-  enabledTransports: isTls ? ["wss"] : ["ws"],
+  enabledTransports: ['ws', 'wss'],
   authEndpoint: authEndpoint,
   authorizer: (channel: { name: string }) => {
     return {
